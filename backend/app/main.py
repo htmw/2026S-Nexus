2b9a3991f6ad011d45e8eb2130311ad9a55e4701
Kris Pothana <105060358+sidcandoall@users.noreply.github.com>
2026-05-14 02:01:29 -0400

    CORS issue resolved

--- backend/app/main.py
+++ backend/app/main.py
@@ -58,6 +58,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://mmirror.vercel.app",
     ],
     allow_origin_regex=r"http://(localhost|127\.0\.0\.1)(:\d+)?",
     allow_credentials=True,
